b4280245f4050a505c408e4286cf3309d12ebde4
Heydosomething <bh939@naver.com>
2021-04-01 17:01:49 +0900

    소수

---
+++ Baekjoon/기본수학2/소수.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool check(int n) {
+	if (n == 1)
+		return false;
+	for (int j = 2; j <= sqrt(n); j++) {
+		if (n % j == 0) return false;
+	}
+	return true;
+}
+int main() {
+	int first, second;
+	cin >> first >> second;
+	int arr[10001];
+	int n = 0;
+
+	for (int i = first; i <= second; i++) {
+		if (check(i)){
+			arr[n] = i;
+			n++;
+		}
+	}
+	if (n == 0)
+		cout << -1 << endl;
+	else {
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += arr[i];
+		}
+		cout << sum << endl;
+		cout << arr[0] << endl;
+	}
+}
