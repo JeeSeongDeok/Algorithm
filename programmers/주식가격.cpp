6806efdabf6c1a7e0893c55f88cb8dca29a14b4d
Heydosomething <bh939@naver.com>
2021-04-05 13:05:05 +0900

    프로그래머스 레벨2

---
+++ programmers/주식가격.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    for(int i = 0; i < prices.size(); i++){
+        int time = 0;
+        for(int j = i + 1; j < prices.size(); j++){
+            if(prices[i] > prices[j]){
+                time++;
+                break;
+            }
+            time++;
+        }
+        answer.push_back(time);
+    }
+    return answer;
+}
