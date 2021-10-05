9ccd9eb37ef755a8de2a590c9967d4d08e7c74ed
JeeSeongDeok <bh939@naver.com>
2021-10-05 23:44:11 +0900

    Update 베스트앨범.kt

--- programmers/Level_2/베스트앨범.kt
+++ programmers/Level_2/베스트앨범.kt
@@ -1,23 +1,10 @@
 class Solution {
     fun solution(genres: Array<String>, plays: IntArray): IntArray {
-        var answer = intArrayOf()
-        var genresPlayMap = mutableMapOf<String, Int>()
-        var playCountMap = mutableMapOf<String, HashMap<Int, Int>>()
-        for(i in genres.indices){
-            if(genresPlayMap[genres[i]] == null) {
-                genresPlayMap[genres[i]] = plays[i]
-                playCountMap[genres[i]][plays[i]] = i
-            }
-            else {
-                genresPlayMap[genres[i]] = genresPlayMap[genres[i]]!!.plus(plays[i])
-            }
-        }
-        var sortMap = genresPlayMap.toList().sortedWith(compareByDescending {it.second}).toMap()
-        playCountMap
-        for(i in sortMap){
-            
-        }
-        
-        return answer
+        return genres.indices.groupBy { genres[it] }
+            .toList()
+            .sortedByDescending { it.second.sumBy { plays[it] } }
+            .map { it.second.sortedByDescending { plays[it] }.take(2) }
+            .flatten()
+            .toIntArray()
     }
 }
