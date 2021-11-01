18c36abdbd90642579b42d0aac518d9fd2771776
JeeSeongDeok <bh939@naver.com>
2021-11-01 21:08:33 +0900

    Update 수식최대화 미해결.kt

--- programmers/Level_2/수식최대화 미해결.kt
+++ programmers/Level_2/수식최대화 미해결.kt
@@ -1,4 +1,3 @@
-import kotlin.math.exp
 
 class Solution {
     var str:String = ""
@@ -28,34 +27,54 @@ class Solution {
         for(i in token.indices){
             // Out of Index 일 시
             if(i+1 == token.size) { break }
-            var firstNum: String = ""
-            var secondNum: String = ""
+            var firstNum: Int = 0
+            var secondNum: Int = 0
+            var number: String = ""
             // 피 연산자 앞에 꺼
-            for(ch in token[i].reversed()){
-                if(ch in '0'..'9'){ firstNum += ch.toString() }
-                else { break }
+            for(cIndex in token[i].indices.reversed()){
+                var ch = token[i][cIndex]
+                if(ch in '0'..'9'){ number += ch.toString() }
+                else if (ch == '-' && token[i].getOrNull(cIndex-1) == '-') {
+                    number.reversed().toInt() * -1
+                    break
+                }
+                else {
+                    number.reversed().toInt()
+                    break
+                }
             }
+            number = ""
             // 피 연산자 뒤에 꺼
-            for(ch in token[i+1]){
-                if(ch in '0'..'9'){ secondNum += ch.toString() }
-                else { break }
+            for(cIndex in token[i+1].indices){
+                var ch = token[i][cIndex]
+                if(ch in '0'..'9'){ number += ch.toString() }
+                else if (ch == '-' && token[i].getOrNull(cIndex+1) == '-') {
+                    number.toInt() * -1
+                    break
+                }
+                else {
+                    number.toInt()
+                    break
+                }
             }
+            println("$firstNum, $secondNum")
             // 연산자 위치 확인 후 스트링 자르기
             when(operation){
                 "-"->{
-                    result = firstNum.reversed().toInt() - secondNum.toInt()
+                    result = firstNum - secondNum.toInt()
                 }
                 "*"->{
-                    result = firstNum.reversed().toInt() * secondNum.toInt()
+                    result = firstNum * secondNum.toInt()
                 }
                 "+"->{
-                    result = firstNum.reversed().toInt() + secondNum.toInt()
+                    result = firstNum + secondNum.toInt()
                 }
             }
-            str = str.replaceFirst("${firstNum.reversed()}$operation$secondNum", result.toString())
+            str = str.replaceFirst("${firstNum}$operation$secondNum", result.toString())
             println(str)
         }
     }
+
 }
 fun main(){
     Solution().solution("100-200*300-500+20")
