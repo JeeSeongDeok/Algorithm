9877eb466f0dd3b2febc22c0010d7ee03dda92ca
JeeSeongDeok <bh939@naver.com>
2021-12-16 17:53:22 +0900

    Update 수식최대화 미해결.kt

--- programmers/Level_2/수식최대화 미해결.kt
+++ programmers/Level_2/수식최대화 미해결.kt
@@ -1,81 +1,44 @@
-
 class Solution {
-    var str:String = ""
+    fun calculator(
+        number: MutableList<String>,
+        operator: MutableList<Char>,
+        firstChar: Char, secondChar: Char,
+        thirdChar: Char
+    ): Long {
+        var result: Long = 0
+        // 우선순위
+        while (operator.indexOf(firstChar) != null) {
+            
+        }
+
+
+        return result
+    }
+
     fun solution(expression: String): Long {
         var answer: Long = 0
-
-        str = expression
-
-        var token = str.split('*')
-        call(token, "*")
-        token = str.split('+')
-        call(token, "+")
-        token = str.split("-")
-        call(token, "-")
-        println(str)
-        // - + *
-
-        // * + -
-
-        // * - +
-
+        // 숫자들의 모임
+        var numberList = expression
+            .replace("-", " ")
+            .replace("+", " ")
+            .replace("*", " ")
+            .split(" ")
+            .toMutableList()
+        // 오퍼레이터
+        val operatorList = mutableListOf<Char>()
+        var index = 0
+        numberList.forEach {
+            index += it.length
+            if (index + operatorList.size < expression.length) {
+                val operator = expression[index + operatorList.size]
+                operatorList.add(operator)
+            }
+        }
+        calculat(expression)
         return answer
     }
-    fun call(token:List<String>, operation:String){
-        var result = 0
-
-        for(i in token.indices){
-            // Out of Index 일 시
-            if(i+1 == token.size) { break }
-            var firstNum: Int = 0
-            var secondNum: Int = 0
-            var number: String = ""
-            // 피 연산자 앞에 꺼
-            for(cIndex in token[i].indices.reversed()){
-                var ch = token[i][cIndex]
-                if(ch in '0'..'9'){ number += ch.toString() }
-                else if (ch == '-' && token[i].getOrNull(cIndex-1) == '-') {
-                    number.reversed().toInt() * -1
-                    break
-                }
-                else {
-                    number.reversed().toInt()
-                    break
-                }
-            }
-            number = ""
-            // 피 연산자 뒤에 꺼
-            for(cIndex in token[i+1].indices){
-                var ch = token[i][cIndex]
-                if(ch in '0'..'9'){ number += ch.toString() }
-                else if (ch == '-' && token[i].getOrNull(cIndex+1) == '-') {
-                    number.toInt() * -1
-                    break
-                }
-                else {
-                    number.toInt()
-                    break
-                }
-            }
-            println("$firstNum, $secondNum")
-            // 연산자 위치 확인 후 스트링 자르기
-            when(operation){
-                "-"->{
-                    result = firstNum - secondNum.toInt()
-                }
-                "*"->{
-                    result = firstNum * secondNum.toInt()
-                }
-                "+"->{
-                    result = firstNum + secondNum.toInt()
-                }
-            }
-            str = str.replaceFirst("${firstNum}$operation$secondNum", result.toString())
-            println(str)
-        }
-    }
-
 }
-fun main(){
-    Solution().solution("100-200*300-500+20")
+
+fun main() {
+    println(Solution().solution("100-200*300-500+20"))
 }
